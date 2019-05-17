79284fe6f6709731fcc9ff05f7b784008ad36b63
Florian Wyss <fw@studer-raimann.ch>
2019-05-17 09:46:03 +0200

    Use ilDBConstants

--- vendor/srag/notifications4plugin/src/Notification/Repository.php
+++ vendor/srag/notifications4plugin/src/Notification/Repository.php
@@ -3,7 +3,6 @@
 namespace srag\Notifications4Plugin\SrAutoMails\Notification;
 
 use ilDateTime;
-use ilDBConstants;
 use srag\DIC\SrAutoMails\DICTrait;
 use srag\DIC\SrAutoMails\Plugin\PluginInterface;
 use srag\Notifications4Plugin\SrAutoMails\Ctrl\CtrlInterface;
@@ -230,7 +229,7 @@ final class Repository implements RepositoryInterface {
 					}
 
 					$result2 = self::dic()->database()->queryF("SELECT * FROM " . $global_plugin_notification_language_table_name
-						. " WHERE notification_id=%s", [ ilDBConstants::T_INTEGER ], [ $row["id"] ]);
+						. " WHERE notification_id=%s", [ "integer" ], [ $row["id"] ]);
 
 					while (($row2 = $result2->fetchAssoc()) !== false) {
 						$notification->setSubject($row2["subject"], $row2["language"]);
